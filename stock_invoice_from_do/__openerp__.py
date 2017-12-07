40a98be0670a8dcd15de8c8c76153fb901c07a03
Andhitia Rama <andhitia.r@gmail.com>
2017-12-07 11:10:20 +0700

    [8.0.1.0.0] stock_invoice_from_do

---
+++ stock_invoice_from_do/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Invoice From DO instead of Customer Reception",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Stock Management",
+    "depends": [
+        "stock_route_transit",
+        "sale_stock",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
